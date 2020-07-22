cf158ab1a59c1202fd058f54bbe906c43bc55ea9
Octaniel <otanieljose30@gmail.com>
2020-07-22 18:44:28 +0000

    V0.3

--- lib/app/res/static.dart
+++ lib/app/res/static.dart
@@ -1,2 +1,3 @@
-final String url = "http://localhost:8080/";
+final String url = "https://gestoreferencia-pagamento-api.herokuapp.com/";
+final String url1 = "http://localhost:8080/";
 final String url2 = "https://gestoreferencia-pagamento-api.herokuapp.com/";
